1403a8ad32fb8781f13ef7ffff827bf0eeeb8bc8
amasports-eu <amasport8889@gmail.com>
2017-09-05 21:43:48 +0200

    form prova 2

--- contactus.php
+++ contactus.php
@@ -1,17 +1,19 @@
 <?php
-session_start();
-error_reporting(1);
-include("PHPMailer/PHPMailerAutoload.php");
+session_cache_limiter('nocache');
+header('Expires: ' . gmdate('r', 0));
+
+header('Content-type: application/json');
+
+require_once('PHPMailer/PHPMailerAutoload.php');
 
 // Step 1 - Enter your email address below.
-$to =  'info@amasports.eu'; //'info@bathwestfire.co.uk';
+$email = 'info@amasports.eu';     //'.com'; 
 
-if(isset($_POST['emailSent'])) {
-
-	// Step 2 - If you don't want a "captcha" verification, remove that IF.
+// If the e-mail is not working, change the debug option to 2 | $debug = 2;
+$debug = 0;
 
 		$name = $_POST['name'];
-		$email = $_POST['email'];
+		$eemail = $_POST['email'];
 
 		// Step 3 - Configure the fields list that you want to receive on the email.
 		$fields = array(
@@ -33,12 +35,34 @@ if(isset($_POST['emailSent'])) {
 			)
 		);
 
-		$message = "";
 
-		foreach($fields as $field) {
-			$message .= $field['text'].": " . htmlspecialchars($field['val'], ENT_QUOTES) . "<br>\n";
-		}
+$message = '';
+
+foreach($fields as $field) {
+	$message .= $field['text'].": " . htmlspecialchars($field['val'], ENT_QUOTES) . "<br>\n";
+}
+
+$mail = new PHPMailer;
+
+try {
+	$mail->SMTPDebug = $debug;                                 // Debug Mode
+    
+    /*
     $mail = new PHPMailer;
+
+$mail->isSMTP();                                   // Set mailer to use SMTP
+$mail->Host = 'smtp.gmail.com';                    // Specify main and backup SMTP servers
+$mail->SMTPAuth = true;                            // Enable SMTP authentication
+$mail->Username = 'ml.com';          // SMTP username
+$mail->Password = 'He56'; // SMTP password
+$mail->SMTPSecure = 'tls';                         // Enable TLS encryption, `ssl` also accepted
+$mail->Port = 587;     
+    */
+    
+    
+    
+	// Step 2 (Optional) - If you don't receive the email, try to configure the parameters below:
+
 	$mail->IsSMTP();                                         // Set mailer to use SMTP
 	$mail->Host = 'smtp.gmail.com';				       // Specify main and backup server
 	$mail->SMTPAuth = true;                                  // Enable SMTP authentication
@@ -47,27 +71,25 @@ if(isset($_POST['emailSent'])) {
 	$mail->SMTPSecure = 'tls';                               // Enable encryption, 'ssl' also accepted
 	$mail->Port = 587;   								       // TCP port to connect to
 
-	$mail->AddAddress($to);	 						       // Add another recipient
+	$mail->AddAddress($email);	 						       // Add another recipient
 
-	$mail->SetFrom($email, $_POST['name']);
+	$mail->SetFrom($eemail, $_POST['name']);
 	$mail->AddReplyTo($_POST['email'], $_POST['name']);
 
 	$mail->IsHTML(true);                                  // Set email format to HTML
 
 	$mail->CharSet = 'UTF-8';
-
-	$mail->Subject = $subject;
 	$mail->Body    = $message;
 
 	$mail->Send();
-
-		if($mail->Send()) {
-			$arrResult = array('response'=> 'success');
-            echo "Successfully sent";
-		} else {
-			$arrResult = array('response'=> 'error', 'error'=> $mail->ErrorInfo);
-            echo $arrResult;
-		}
-
-	}
+	$arrResult = array ('response'=>'success');
+    if($mail->Send())
+    echo "Mail Successfully sent";
+    else
+        echo $mail->ErrorInfo;
+} catch (phpmailerException $e) {
+	$arrResult = array ('response'=>'error','errorMessage'=>$e->errorMessage());
+} catch (Exception $e) {
+	$arrResult = array ('response'=>'error','errorMessage'=>$e->getMessage());
+}
 ?>
